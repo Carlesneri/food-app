02b65c3f918999375f3610690f919733f6b4f354
carlos.company <carlos.company@nexum.com>
2024-11-01 13:25:19 +0100

    Add meta tag for google search

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,34 @@
-
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx)
+		return { ...initialProps }
+	}
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Kufam:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="icon" href="/favicon.ico" />
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Kufam:ital,wght@0,400;0,600;1,400&display=swap"
+						rel="stylesheet"
+					/>
+					<meta
+						name="google-site-verification"
+						content="m5v5WcaaiigGVIGuIOa8t6LTmF1UUOZtxiq2Nys-5V8"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
 }
 
 export default MyDocument
